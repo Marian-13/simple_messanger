0fe2f535f4a685d84af88e87ea224198e893542b
Marian-13 <marian144519@gmail.com>
2016-09-23 14:27:28 +0300

    user validation

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'users', to: 'users#index'
-  post 'users', to: 'users#create'
-  get 'users/new', to: 'users#new'
-  get 'users/:id', to: 'users#show'
 end
