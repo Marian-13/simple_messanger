c089b6295448ac6cd08cce02dde3edaf3e6dc373
Marian-13 <marian144519@gmail.com>
2016-09-23 13:29:56 +0300

    user resource without scaffold

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(
+  [
+    { name: 'User01', email: 'user01@email.com' },
+    { name: 'User02', email: 'user02@email.com' },
+    { name: 'User03', email: 'user03@email.com' },
+    { name: 'User04', email: 'user04@email.com' },
+    { name: 'User05', email: 'user05@email.com' },
+    { name: 'User06', email: 'user06@email.com' },
+    { name: 'User07', email: 'user07@email.com' },
+    { name: 'User08', email: 'user08@email.com' },
+    { name: 'User09', email: 'user09@email.com' },
+    { name: 'User10', email: 'user10@email.com' }
+  ]
+)
